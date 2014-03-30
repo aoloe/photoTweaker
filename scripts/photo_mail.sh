640c2f00bac8a103820a943ff3a8354bd9d8e43b
a.l.e <ale@ideale.ch>
2014-03-30 17:59:39 +0200

    add the script for mailing the photos

---
+++ scripts/photo_mail.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+filename=$(tempfile --suffix ".jpg");
+echo "filename: " $filename
+
+mount /mnt/nikon 2> /dev/null
+#echo $q
+
+q=$(ls -tr1 /mnt/nikon/dcim 2> /dev/null | tail -n 1)
+
+if [ "$q" != "" ]; then
+    r=$(ls -tr1 /mnt/nikon/dcim/$q 2> /dev/null | tail -n 1)
+    #echo $r
+    if [ "$q" != "" ]; then
+        # copy or move each token to /tmp
+        for item in $r; do
+            echo $item
+            cp /mnt/nikon/dcim/$q/$item $filename
+        done
+    else
+        echo "could not find any image"
+    fi
+else
+    echo "could not mount the sd card"
+fi
+
+umount /mnt/nikon 2> /dev/null
+
+
+~/docs/src/photoTweaker/bin/photoTweaker $filename
+
+claws-mail --compose --attach $filename
+
+# rm $filename
