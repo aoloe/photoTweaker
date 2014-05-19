b9f4f03907c920d1bd9dae5b37efeac128722d0b
Ale Rimoldi <ale@graphicslab.org>
2014-05-19 12:55:43 +0200

    add a script for storing screenshots on the disk

---
+++ scripts/screenshot_store.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+filename=$(tempfile --suffix ".png");
+echo "filename: " $filename
+
+import -window root $filename
+
+target_filename=`zenity --file-selection --save --confirm-overwrite`
+
+case $? in
+         0)
+                echo "\"$target_filename\" selected."
+                # TODO: make sure that the filename is .png
+                mv $filename $target_filename
+                ~/docs/src/photoTweaker/bin/photoTweaker $target_filename;;
+         1)
+                echo "No file selected.";;
+        -1)
+                echo "An unexpected error has occurred.";;
+esac
+
+rm $filename
